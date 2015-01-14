dd2524ebc0e28a9002da9e227ebcf5e2f81833f9
hLynx <hlynx@ya.ru>
2015-01-14 18:36:29 +0400

    Minor changes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# picolo
-Picolo CMF
+# Picolo CMF
+
+Installation:
+composer create-project hlynx/picolo <project_name>
+
+For production in htdocs/.htaccess should NOT be 
+SetEnv APP_ENV dev
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -56,5 +56,20 @@ $app->get('{url}', function($url) {
     var_dump($url);
 });
  */
+/*
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+// 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/' . $code . '.html',
+        'errors/' . substr($code, 0, 2) . 'x.html',
+        'errors/' . substr($code, 0, 1) . 'xx.html',
+        'errors/default.html',
+    );
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
+*/
 
 return $app;
